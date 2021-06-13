60e5c6f515692668ec4f4f5bef71f95d43ae8df7
sarthakchauhan <schauh04>
2021-06-13 19:27:16 +0530

    added data persistence

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-var _ = require('lodash');
+const _ = require('lodash');
+const mongoose = require("mongoose");
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,14 +18,29 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let posts = [];
+// let posts = [];
+
+// mongoose database connection
+mongoose.connect('mongodb://localhost:27017/posts', { useNewUrlParser: true, useUnifiedTopology: true });
+
+//Post schema
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+// Post model
+const Post = new mongoose.model("post", postSchema);
 
 
 app.get("/", (req, res) => {
-  // console.log(posts);
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
+  Post.find({}, (err, posts) => {
+    if (!err) {
+      res.render("home", {
+        startingContent: homeStartingContent,
+        posts: posts
+      });
+    }
   });
 });
 
@@ -40,20 +57,37 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
-  };
-  posts.push(post);
-  res.redirect("/");
+  });
+  // posts.push(post);
+  post.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("post added successfully!");
+      res.redirect("/");
+    }
+  });
+  // res.redirect("/");
 });
 
-app.get("/posts/:postName", (req, res) => {
-  const formattedRequestedTitle = _.lowerCase(req.params.postName);  // _ stands for a Lodash object
-  posts.forEach(post => {
-    let formattedGivenTitle = _.lowerCase(post.title);  // _ stands for a Lodash object
-    if (formattedGivenTitle === formattedRequestedTitle) {
-      res.render("post", {post: post});
+// app.get("/posts/:postName", (req, res) => {
+//   const formattedRequestedTitle = _.lowerCase(req.params.postName);  // _ stands for a Lodash object
+//   posts.forEach(post => {
+//     let formattedGivenTitle = _.lowerCase(post.title);  // _ stands for a Lodash object
+//     if (formattedGivenTitle === formattedRequestedTitle) {
+//       res.render("post", { post: post });
+//     }
+//   });
+// });
+
+app.get("/posts/:postId", (req, res) => {
+  const requestedId = req.params.postId;
+  Post.findOne({ _id: requestedId }, (err, post) => {
+    if (!err) {
+      res.render("post", { post: post });
     }
   });
 });
